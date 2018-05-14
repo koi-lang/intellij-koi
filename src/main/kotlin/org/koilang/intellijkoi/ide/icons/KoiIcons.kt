cc788bdf1075b36394263dbb925383f825f8e8d9
DeflatedPickle <DeflatedPickle@gmail.com>
2018-05-14 22:15:19 +0100

    Updated koi_file icon

--- src/main/kotlin/org/koilang/intellijkoi/ide/icons/KoiIcons.kt
+++ src/main/kotlin/org/koilang/intellijkoi/ide/icons/KoiIcons.kt
@@ -3,5 +3,5 @@ package org.koilang.intellijkoi.ide.icons
 import com.intellij.openapi.util.IconLoader
 
 object KoiIcons {
-    val KOI_FILE = IconLoader.getIcon("/main/resources/icons/koi_file.svg")
+    val KOI_FILE = IconLoader.getIcon("/icons/koi_file.svg")
 }
